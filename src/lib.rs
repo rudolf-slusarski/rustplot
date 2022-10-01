b6e3f60a28eb55b8b39ff746f54eeae0f4cfce03
rudolf <rudolf.slusarski@gmail.com>
2022-10-01 17:01:42 +0200

    counting brackets is useless

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,6 @@ impl Equation {
         }
     }
 
-    /// checks validity of parentheses in the equation by counting
-    fn check_parentheses(&self) -> bool {
-        self.formula.matches('(').count() == self.formula.matches(')').count()
-            && self.formula.matches('[').count() == self.formula.matches(']').count()
-    }
 }
 
 enum SpecialFunction {
