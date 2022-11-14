ef57fba6a60a5799d4529eed0d8a15f6c9a8e38a
rudolf <rudolf.slusarski@gmail.com>
2022-11-14 12:44:52 +0100

    axis struct

--- src/axis.rs
+++ src/axis.rs
@@ -2,3 +2,19 @@ enum Direction {
     Vertical,
     Horizontal,
 }
+
+struct Axis {
+    direction: Direction,
+    range: (f64, f64),
+    size: (u32, u32),
+}
+
+impl Axis {
+    fn new(direction: Direction, range: (f64, f64), size: (u32, u32)) -> Self {
+        Axis {
+            direction,
+            range,
+            size,
+        }
+    }
+}
